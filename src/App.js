bd154aa56455050e6dd84b4e582a7b663efdf017
sakuraikeda24 <sakuraikeda24@gmail.com>
2026-03-21 22:02:48 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -367,7 +367,7 @@ export default function MusicVideoWebsite() {
 
           <div className="hero-right">
             <img
-              src="https://images.unsplash.com/photo-1516280440614-37939bbacd81?q=80&w=1200&auto=format&fit=crop"
+              src="https://scontent.fakl5-2.fna.fbcdn.net/v/t39.30808-6/633476797_4356902411303888_8408420196503766101_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=13d280&_nc_ohc=N-mjnAF17IMQ7kNvwFiyJyy&_nc_oc=AdpknrVsCn7omsArqJa8TudII7VdCytG8RL0cchBUyHX9DgnonYUMiYffvYP4T_HK0sg7d_TgYB8dIP-0KOCaEiX&_nc_zt=23&_nc_ht=scontent.fakl5-2.fna&_nc_gid=NoXcCt9FgnZqhBuU3_H3tA&_nc_ss=7a30f&oh=00_AfwctOf5m21gVtZoe1tIRpUZQ3AdYJSTWt_SHvsFMvvqyw&oe=69C42246"
               alt="hero"
             />
           </div>
