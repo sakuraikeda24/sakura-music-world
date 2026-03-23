1180dfff90c22dd9f4b2497074e7b885fa2a031a
sakuraikeda24 <sakuraikeda24@gmail.com>
2026-03-23 23:54:46 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1492,15 +1492,15 @@ Vocal / 演唱 / 歌：AI`
 
         <div className="video-card">
   <div className="video-frame-wrap">
- <iframe
+<iframe
   className="video-frame"
-  src={`https://player.bilibili.com/player.html?bvid=${video.bvid}&page=1&autoplay=0`}
-  title={video.title}
+  src="https://player.bilibili.com/player.html?bvid=BV14nAGzMEbA&page=1&autoplay=0"
+  title="Music Video"
   frameBorder="0"
   allow="fullscreen"
   allowFullScreen
   scrolling="no"
-    />
+/>
   </div>
 </div>
         </section>
